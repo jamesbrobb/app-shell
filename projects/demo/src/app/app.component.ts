d0bf680c587af2609500d42f9d7cdbd4ab2696a9
jamesbrobb <jamesbenrobb@gmail.com>
2024-03-25 10:51:41 +0000

    added search panel component

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import {AppShellLayoutComponent} from "@jamesbenrobb/app-shell";
+import {MatButton} from "@angular/material/button";
+import {OverlayModule, Overlay} from "@angular/cdk/overlay";
+import {ComponentPortal} from "@angular/cdk/portal";
+import {SearchPanelComponent} from "./components/search-panel/search-panel.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    AppShellLayoutComponent
+    AppShellLayoutComponent,
+    MatButton,
+    OverlayModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+
+  readonly #overlay: Overlay;
+  constructor(overlay: Overlay) {
+    this.#overlay = overlay;
+  }
+  showOverlay() {
+    const overlayRef = this.#overlay.create();
+    const searchPortal = new ComponentPortal(SearchPanelComponent);
+    overlayRef.attach(searchPortal);
+  }
+}
